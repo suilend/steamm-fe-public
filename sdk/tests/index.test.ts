4ecbaef26643e851717d7e64f90c53dd2dc0c146
0xxgen1 <0xxgen@solend.fi>
2025-01-21 21:49:24 +0000

    init sdk

---
+++ sdk/tests/index.test.ts
@@ -0,0 +1,10 @@
+import { test as testonTest } from "./teston.test";
+
+describe("Test Suite", () => {
+  // Run all tests from teston.test.ts
+  testonTest();
+
+  // Add more test imports and calls here
+  // import { test as anotherTest } from './another.test';
+  // anotherTest();
+});
