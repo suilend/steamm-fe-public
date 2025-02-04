f854112618ef173fdf4eba158304aded55fb7dfe
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-02-04 18:12:21 +0800

    add colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
     colors: {
       foreground: "hsl(var(--foreground))",
       background: "hsl(var(--background))",
+
+      "jordy-blue": "hsl(var(--jordy-blue))",
+      "rich-black": "hsl(var(--rich-black))",
+      emerald: "hsl(var(--emerald))",
+      "rich-black-light": "hsl(var(--rich-black-light))",
+      "medium-slate-blue": "hsl(var(--medium-slate-blue))",
+      white: "hsl(var(--white))",
     },
     borderRadius: {},
     boxShadow: {},
