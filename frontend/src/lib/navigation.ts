7a0af9028e41200d4e403adbd41464a9b254c172
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-02-10 10:33:54 +0800

    don't show countdown on dev

--- frontend/src/lib/navigation.ts
+++ frontend/src/lib/navigation.ts
@@ -1,9 +1,6 @@
 export const ROOT_URL = "/";
 export const ADMIN_URL = "/admin";
 
-export const SUILEND_URL = "https://suilend.fi";
-export const STEAMM_URL = "https://steamm.fi";
-
 // export const DOCS_URL =
 //   "https://docs.suilend.fi/springsui/intro-to-springsui-standard";
 export const TWITTER_URL = "https://twitter.com/steammfi";
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -9,7 +9,10 @@ import { ASSETS_URL, TITLE } from "@/lib/constants";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isDev, setIsDev] = useState<boolean>(false);
+
   useEffect(() => {
+    setIsDev(window.location.href.includes("localhost"));
     const timeout = setTimeout(() => {
       setIsLoading(false);
     }, 250);
@@ -28,7 +31,7 @@ export default function Home() {
       <div className="flex h-dvh w-full flex-col items-center justify-center">
         {isLoading ? (
           <Loader2 className="h-8 w-8 animate-spin text-foreground" />
-        ) : (
+        ) : !isDev ? (
           <div className="flex w-max flex-col items-center gap-6">
             <Image
               src={`${ASSETS_URL}/STEAMM.svg?`}
@@ -40,6 +43,8 @@ export default function Home() {
 
             <Countdown />
           </div>
+        ) : (
+          <p>real page</p>
         )}
       </div>
     </>
