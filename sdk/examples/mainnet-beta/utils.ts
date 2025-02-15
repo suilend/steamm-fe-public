ee4221c26583f14132757da468cee893569d61cc
0xxgen1 <0xxgen@solend.fi>
2025-02-15 03:48:11 +0100

    create pool script

---
+++ sdk/examples/mainnet-beta/utils.ts
@@ -0,0 +1,219 @@
+import {
+  SuiObjectChange,
+  SuiTransactionBlockResponse,
+} from "@mysten/sui/client";
+import {
+  Transaction,
+  TransactionArgument,
+  TransactionObjectInput,
+  TransactionResult,
+} from "@mysten/sui/transactions";
+import { normalizeSuiAddress } from "@mysten/sui/utils";
+
+import { SteammSDK, SuiAddressType } from "../../src";
+import { createCoinBytecode, getTreasuryAndCoinMeta } from "../../src/coinGen";
+
+export interface CoinData {
+  coinMeta: string;
+  coinType: string;
+  btreasury: string;
+  bTokenmeta: string;
+  btokenType: string;
+  bTokenSymbol: string;
+}
+
+export interface LpData {
+  lpTreasuryId: string;
+  lpMetadataId: string;
+  lpTokenType: string;
+}
+
+export async function createCoinTx(
+  tx: Transaction,
+  coinSymbol: string,
+  sender: SuiAddressType,
+): Promise<Transaction> {
+  const bytecode = await createCoinBytecode(
+    coinSymbol.toUpperCase().replace(/\s+/g, "_"),
+    coinSymbol.toLowerCase().replace(/\s+/g, "_"),
+    coinSymbol,
+    `Coin${coinSymbol}`,
+    `Coin ${coinSymbol}`,
+    "",
+  );
+
+  // Step 1: Create the coin
+  const [upgradeCap] = tx.publish({
+    modules: [[...bytecode]],
+    dependencies: [normalizeSuiAddress("0x1"), normalizeSuiAddress("0x2")],
+  });
+
+  tx.transferObjects([upgradeCap], tx.pure.address(sender));
+
+  return tx;
+}
+
+export async function createCoinHelper(
+  sdk: SteammSDK,
+  coinSymbol: string,
+  tx: Transaction = new Transaction(),
+): Promise<[string, string, string]> {
+  await createCoinTx(tx, coinSymbol, sdk.senderAddress);
+
+  const txResponse = await sdk.fullClient.signAndExecuteTransaction({
+    transaction: tx,
+    signer: sdk.signer!,
+    options: {
+      showEvents: true,
+      showEffects: true,
+      showObjectChanges: true,
+    },
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, 500));
+
+  return getTreasuryAndCoinMeta(txResponse);
+}
+
+export async function createBankAndBTokenHelper(
+  sdk: SteammSDK,
+  coinSymbol: string,
+  coinType: string,
+  coinMeta: string,
+): Promise<CoinData> {
+  const [btreasury, bTokenmeta, btokenType] = await createBTokenHelper(
+    sdk,
+    coinSymbol,
+    coinType,
+    coinMeta,
+  );
+
+  const bTokenSymbol = `b${coinSymbol}`;
+
+  await new Promise((resolve) => setTimeout(resolve, 500));
+
+  return {
+    coinMeta,
+    coinType,
+    btreasury,
+    bTokenmeta,
+    btokenType,
+    bTokenSymbol,
+  };
+}
+
+export async function createBTokenHelper(
+  sdk: SteammSDK,
+  coinSymbol: string,
+  coinType: string,
+  coinMeta: string,
+): Promise<[string, string, string]> {
+  const tx = await sdk.Bank.createBToken(
+    coinType,
+    coinSymbol,
+    sdk.senderAddress,
+  );
+
+  const coinTxResponse = await sdk.fullClient.signAndExecuteTransaction({
+    transaction: tx,
+    signer: sdk.signer!,
+    options: {
+      showEvents: true,
+      showEffects: true,
+      showObjectChanges: true,
+    },
+  });
+
+  const [bTokenTreasuryId, bTokenMetadataId, bTokenTokenType] =
+    getTreasuryAndCoinMeta(coinTxResponse);
+
+  const newBankTx = new Transaction();
+
+  await sdk.Bank.createBank(newBankTx, coinTxResponse, {
+    coinType: coinType,
+    coinMetaT: coinMeta,
+  });
+
+  const newBankTxResponse = await sdk.fullClient.signAndExecuteTransaction({
+    transaction: newBankTx,
+    signer: sdk.signer!,
+    options: {
+      showEvents: true,
+      showEffects: true,
+      showObjectChanges: true,
+    },
+  });
+
+  return [bTokenTreasuryId, bTokenMetadataId, bTokenTokenType];
+}
+
+export async function createPoolHelper(
+  sdk: SteammSDK,
+  coinAData: CoinData,
+  coinBData: CoinData,
+): Promise<LpData> {
+  const tx = await sdk.Pool.createLpToken(
+    coinAData.bTokenSymbol,
+    coinBData.bTokenSymbol,
+    sdk.senderAddress,
+  );
+
+  const coinTxResponse = await sdk.fullClient.signAndExecuteTransaction({
+    transaction: tx,
+    signer: sdk.signer!,
+    options: {
+      showEvents: true,
+      showEffects: true,
+      showObjectChanges: true,
+    },
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, 500));
+
+  const [lpTreasuryId, lpMetadataId, lpTokenType] =
+    getTreasuryAndCoinMeta(coinTxResponse);
+
+  const newPoolTx = new Transaction();
+
+  await sdk.Pool.createPool(newPoolTx, coinTxResponse, {
+    btokenTypeA: coinAData.btokenType,
+    btokenTypeB: coinBData.btokenType,
+    swapFeeBps: BigInt(100),
+    offset: BigInt(0),
+    coinMetaA: coinAData.bTokenmeta,
+    coinMetaB: coinBData.bTokenmeta,
+  });
+
+  const newPoolTxResponse = await sdk.fullClient.signAndExecuteTransaction({
+    transaction: newPoolTx,
+    signer: sdk.signer!,
+    options: {
+      showEvents: true,
+      showEffects: true,
+      showObjectChanges: true,
+    },
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, 500));
+
+  return {
+    lpTreasuryId,
+    lpMetadataId,
+    lpTokenType,
+  };
+}
+
+export function mintCoin(
+  tx: Transaction,
+  coinType: string,
+  treasuryCap: string,
+): TransactionResult {
+  return tx.moveCall({
+    target: "0x2::coin::mint",
+    typeArguments: [coinType],
+    arguments: [
+      tx.object(treasuryCap),
+      tx.pure.u64(BigInt("10000000000000000")),
+    ],
+  });
+}
--- sdk/src/config/mainnetBeta.ts
+++ sdk/src/config/mainnetBeta.ts
@@ -3,12 +3,23 @@ export const SUILEND_BETA_PKG_ID =
 export const SUILEND_BETA_PUBLISHED_AT =
   "0x5bb8cb3894945f523736f4f5059b1621056e8093b165ea56b20805d0ef2461a9";
 export const STEAMM_BETA_PKG_ID =
-  "0x10e03a93cf1e3d676677cd2904b003af1511a2b68127aac333371c930f819cba";
+  "0x6577c4d23783f79ba40d501e29a017c1e36a280ea3036632ba1bfe152dacadf5";
 export const STEAMM_BETA_PUBLISHED_AT =
-  "0x10e03a93cf1e3d676677cd2904b003af1511a2b68127aac333371c930f819cba";
+  "0x6577c4d23783f79ba40d501e29a017c1e36a280ea3036632ba1bfe152dacadf5";
 
 export const REGISTRY_BETA_OBJECT_ID =
-  "0xd9d7266e4052901e511b2d324653e44f1d3854da6985ec5ddcdd810201c0f225";
+  "0x6577c4d23783f79ba40d501e29a017c1e36a280ea3036632ba1bfe152dacadf5";
+
+export const GLOBAL_ADMIN_OBJECT_ID =
+  "0x015d951a430da11f4a997d3058ffea49257045df894c6817b663d28b12c511a6";
+
+export const LENDING_MARKET_ID =
+  "0x84030d26d85eaa7035084a057f2f11f701b7e2e4eda87551becbc7c97505ece1";
+export const LENDING_MARKET_TYPE =
+  "0xf95b06141ed4a174f239417323bde3f209b972f5930d8521ea38a52aff3a6ddf::suilend::MAIN_POOL";
+
+export const STEAMM_SCRIPT_PKG_ID =
+  "0xe167bfdc4b8410e4af76079a37412590ace95064b2d17f1f3b7fed2009a59843";
 
 export const TEST_SUI_BETA_TYPE = `${STEAMM_BETA_PKG_ID}::sui::SUI`;
 export const TEST_USDC_BETA_TYPE = `${STEAMM_BETA_PKG_ID}::usdc::USDC`;
@@ -16,13 +27,28 @@ export const TEST_USDC_BETA_TYPE = `${STEAMM_BETA_PKG_ID}::usdc::USDC`;
 export const STEAMM_BETA_CONFIG = {
   package_id: STEAMM_BETA_PKG_ID,
   published_at: STEAMM_BETA_PUBLISHED_AT,
+  config: {
+    registryId: REGISTRY_BETA_OBJECT_ID,
+    globalAdmin: GLOBAL_ADMIN_OBJECT_ID,
+  },
 };
 export const SUILEND_BETA_CONFIG = {
   package_id: SUILEND_BETA_PUBLISHED_AT,
   published_at: SUILEND_BETA_PKG_ID,
+  config: {
+    lendingMarketId: LENDING_MARKET_ID,
+    lendingMarketType: LENDING_MARKET_TYPE,
+  },
 };
+
+export const STEAMM_SCRIPT_CONFIG = {
+  package_id: STEAMM_SCRIPT_PKG_ID,
+  published_at: STEAMM_SCRIPT_PKG_ID,
+};
+
 export const BETA_CONFIG = {
   fullRpcUrl: "https://fullnode.mainnet.sui.io:443",
   steamm_config: STEAMM_BETA_CONFIG,
   suilend_config: SUILEND_BETA_CONFIG,
+  steamm_script_config: STEAMM_SCRIPT_CONFIG,
 };
