df8b75000d5c9fa330c582a5e0e81ae6468b3e85
0xxgen1 <0xxgen@solend.fi>
2025-02-17 04:00:39 +0100

    fix package ids in codegen

--- frontend/src/fetchers/useFetchAppData.ts
+++ frontend/src/fetchers/useFetchAppData.ts
@@ -127,10 +127,7 @@ export default function useFetchAppData(steammClient: SteammSDK) {
       const coinTypeA = bTokenTypeCoinTypeMap[btokenTypeA];
       const coinTypeB = bTokenTypeCoinTypeMap[btokenTypeB];
 
-      const pool = await steammClient.fullClient.fetchPool(
-        poolInfo.poolId,
-        steammClient.sourcePkgId(),
-      );
+      const pool = await steammClient.fullClient.fetchPool(poolInfo.poolId);
 
       const balanceA = new BigNumber(pool.balanceA.value.toString()).div(
         10 ** poolCoinMetadataMap[coinTypeA].decimals,
