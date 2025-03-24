1407a105f69b16a72d4c3c1a2c38c90861302fa3
0xxgen1 <0xxgen@solend.fi>
2025-03-24 22:28:07 +0000

    updated mainnet package IDs

--- sdk/src/config/mainnet.ts
+++ sdk/src/config/mainnet.ts
@@ -4,7 +4,7 @@ export const STEAMM_CONFIG = {
   package_id:
     "0x4fb1cf45dffd6230305f1d269dd1816678cc8e3ba0b747a813a556921219f261",
   published_at:
-    "0x80e6e13fa97030984f871dcadbec36bcfe02a51225fca97ba60d32ea24c6f0fc",
+    "0x67e4835cbe51818ce79af790f25ee7d8dfb03fc1556094ca5531cc399c687444",
   config: {
     registryId:
       "0x8584948e8c0a2809ec192ede7e030b0a32bd602e5ca6c91bde8dc35fb8b0068d",
@@ -12,7 +12,7 @@ export const STEAMM_CONFIG = {
       "0xdd3d22dba6c38117615a51698136e9867191328a8ef1b065c342d0a887b9be4a",
     quoterSourcePkgs: {
       cpmm: "0x4fb1cf45dffd6230305f1d269dd1816678cc8e3ba0b747a813a556921219f261",
-      omm: "TODO",
+      omm: "0x67e4835cbe51818ce79af790f25ee7d8dfb03fc1556094ca5531cc399c687444",
     },
   },
 };
@@ -38,10 +38,13 @@ export const SUILEND_CONFIG = {
 };
 
 export const ORACLE_CONFIG = {
-  package_id: "TODO",
-  published_at: "TODO",
+  package_id:
+    "0xe84b649199654d18c38e727212f5d8dacfc3cf78d60d0a7fc85fd589f280eb2b",
+  published_at:
+    "0xe84b649199654d18c38e727212f5d8dacfc3cf78d60d0a7fc85fd589f280eb2b",
   config: {
-    oracleRegistryId: "TODO",
+    oracleRegistryId:
+      "0x919bba48fddc65e9885433e36ec24278cc80b56bf865f46e9352fa2852d701bc",
   },
 };
 
