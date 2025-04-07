b8cc5aae80c8caaf5e00c32c2d6b2ba4228dbec3
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-04-07 17:02:16 +0800

    pnl calc fix: split transaction history into separate positions

--- frontend/src/components/pool/TransactionHistoryTable.tsx
+++ frontend/src/components/pool/TransactionHistoryTable.tsx
@@ -1,16 +1,15 @@
-import { CSSProperties, useMemo } from "react";
+import { CSSProperties, Fragment } from "react";
 
 import { ClassValue } from "clsx";
-import { useLocalStorage } from "usehooks-ts";
 
 import TransactionHistoryRow from "@/components/pool/TransactionHistoryRow";
-import HeaderColumn, { SortDirection } from "@/components/TableHeaderColumn";
+import HeaderColumn from "@/components/TableHeaderColumn";
 import { Skeleton } from "@/components/ui/skeleton";
 import { HistoryDeposit, HistoryRedeem } from "@/lib/types";
 import { cn } from "@/lib/utils";
 
 type Column = "date" | "type" | "amounts" | "digest";
-type SortableColumn = "date";
+type SortableColumn = "";
 
 export const columnStyleMap: Record<Column, CSSProperties> = {
   date: {
@@ -35,49 +34,13 @@ export const columnStyleMap: Record<Column, CSSProperties> = {
 
 interface TransactionHistoryTableProps {
   className?: ClassValue;
-  transactionHistory?: (HistoryDeposit | HistoryRedeem)[];
+  transactionHistory?: (HistoryDeposit | HistoryRedeem)[][];
 }
 
 export default function TransactionHistoryTable({
   className,
   transactionHistory,
 }: TransactionHistoryTableProps) {
-  // Sort
-  type SortState = { column: SortableColumn; direction: SortDirection };
-
-  const [sortState, setSortState] = useLocalStorage<SortState | undefined>(
-    `TransactionHistoryTable_sortState`,
-    undefined,
-  );
-
-  const toggleSortByColumn = (column: SortableColumn) => {
-    setSortState((prev) => {
-      if (prev === undefined || prev.column !== column)
-        return { column, direction: SortDirection.DESC };
-
-      if (prev.direction === SortDirection.DESC)
-        return { column, direction: SortDirection.ASC };
-      else return undefined;
-    });
-  };
-
-  const sortedTransactionHistory = useMemo(() => {
-    if (transactionHistory === undefined || sortState === undefined)
-      return transactionHistory;
-
-    return transactionHistory.slice().sort((a, b) => {
-      if (["date"].includes(sortState.column)) {
-        return sortState.direction === SortDirection.DESC
-          ? new Date(+b.timestamp * 1000).getTime() -
-              new Date(+a.timestamp * 1000).getTime()
-          : new Date(+a.timestamp * 1000).getTime() -
-              new Date(+b.timestamp * 1000).getTime();
-      }
-
-      return 0; // Should never reach here
-    });
-  }, [transactionHistory, sortState]);
-
   return (
     <div className="relative w-full">
       <div className="pointer-events-none absolute inset-0 z-[2] border" />
@@ -87,8 +50,6 @@ export default function TransactionHistoryTable({
         <div className="sticky left-0 top-0 z-[2] flex h-[calc(1px+40px+1px)] w-full min-w-max shrink-0 flex-row border bg-secondary">
           <HeaderColumn<Column, SortableColumn>
             id="date"
-            sortState={sortState}
-            toggleSortByColumn={toggleSortByColumn}
             style={columnStyleMap.date}
           >
             Date
@@ -117,23 +78,30 @@ export default function TransactionHistoryTable({
         </div>
 
         {/* Rows */}
-        {sortedTransactionHistory === undefined ? (
+        {transactionHistory === undefined ? (
           Array.from({ length: 3 }).map((_, index) => (
             <Skeleton
               key={index}
               className="relative z-[1] h-[calc(44px+1px)] w-full border-x border-b"
             />
           ))
-        ) : sortedTransactionHistory.length === 0 ? (
+        ) : transactionHistory.length === 0 ? (
           <div className="flex h-[calc(44px+1px)] w-full flex-row items-center justify-center border-x border-b bg-background">
             <p className="text-p2 text-tertiary-foreground">No transactions</p>
           </div>
         ) : (
-          sortedTransactionHistory.map((transaction) => (
-            <TransactionHistoryRow
-              key={transaction.id}
-              transaction={transaction}
-            />
+          transactionHistory.map((positionTransactionHistory, index) => (
+            <Fragment key={index}>
+              {positionTransactionHistory.map((transaction) => (
+                <TransactionHistoryRow
+                  key={transaction.id}
+                  transaction={transaction}
+                />
+              ))}
+              {index !== transactionHistory.length - 1 && (
+                <div className="h-[3px] w-full bg-border" />
+              )}
+            </Fragment>
           ))
         )}
       </div>
--- frontend/src/pages/portfolio.tsx
+++ frontend/src/pages/portfolio.tsx
@@ -61,7 +61,7 @@ export default function PortfolioPage() {
                 if (!pool) return acc;
 
                 const depositedAmounts = [0, 1].map((index) =>
-                  transactionHistory.reduce(
+                  transactionHistory[0].reduce(
                     (acc, entry) =>
                       entry.type === HistoryTransactionType.DEPOSIT
                         ? acc.plus(
