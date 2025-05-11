7a41ddeb2e3700774bc12ca177e0b8b1b5214503
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-05-11 23:41:43 +0800

    hide additional fee tiers

--- frontend/src/lib/createPool.ts
+++ frontend/src/lib/createPool.ts
@@ -22,9 +22,9 @@ import { BURN_ADDRESS } from "@/lib/launchToken";
 import { QUOTER_ID_NAME_MAP, QuoterId } from "@/lib/types";
 
 export const AMPLIFIERS: number[] = [1, 5, 10, 20, 30, 50, 100];
-export const FEE_TIER_PERCENTS: number[] = [
-  1, 5, 10, 20, 25, 30, 50, 100, 200,
-].map((bps) => bps / 100);
+export const FEE_TIER_PERCENTS: number[] = [1, 30, 100, 200].map(
+  (bps) => bps / 100,
+);
 
 const getBTokenModule = (token: Token) =>
   `b_${token.coinType.split("::")[1]}`.toLowerCase(); // E.g. b_sui
