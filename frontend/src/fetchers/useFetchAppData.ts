9e0f1b46aeb567d3c328e22717a7fc08754ac506
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-05-13 10:14:21 +0800

    use new lst info endpoint

--- frontend/src/fetchers/useFetchAppData.ts
+++ frontend/src/fetchers/useFetchAppData.ts
@@ -199,15 +199,18 @@ export default function useFetchAppData(
 
       // LSTs
       (async () => {
-        const lstAprsRes = await fetch(`${API_URL}/springsui/all`);
-        const lstAprsJson: Record<string, string> = await lstAprsRes.json();
-        if ((lstAprsRes as any)?.statusCode === 500)
-          throw new Error("Failed to fetch SpringSui LST APRs");
+        const lstInfoRes = await fetch(`${API_URL}/springsui/lst-info`);
+        const lstInfoJson: Record<
+          string,
+          { LIQUID_STAKING_INFO: any; liquidStakingInfo: any; apy: string }
+        > = await lstInfoRes.json();
+        if ((lstInfoRes as any)?.statusCode === 500)
+          throw new Error("Failed to fetch SpringSui LST data");
 
         return Object.fromEntries(
-          Object.entries(lstAprsJson).map(([coinType, aprPercent]) => [
+          Object.entries(lstInfoJson).map(([coinType, lstData]) => [
             coinType,
-            new BigNumber(aprPercent),
+            new BigNumber(lstData.apy),
           ]),
         ) as AppData["lstAprPercentMap"];
       })(),
