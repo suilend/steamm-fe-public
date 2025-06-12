b655238d47cf4f333eae720cc0bd0f186b4a246d
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-06-12 19:51:43 +0800

    pools page max width

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -232,7 +232,7 @@ export default function PoolsPage() {
         <title>STEAMM</title>
       </Head>
 
-      <div className="flex w-full flex-col gap-8">
+      <div className="flex w-full max-w-6xl flex-col gap-8">
         {/* Stats */}
         <div className="flex w-full flex-row items-stretch rounded-md border">
           {/* TVL */}
