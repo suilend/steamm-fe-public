e1082d3654a76adccefd4835d660ca2e61aec8d8
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-06-25 15:57:38 +0800

    fix

--- frontend/src/components/airdrop/AirdropCard.tsx
+++ frontend/src/components/airdrop/AirdropCard.tsx
@@ -202,7 +202,12 @@ export default function AirdropCard() {
       }
 
       // 1.2) Check
-      await checkIfKeypairCanBeUsed(currentFlowDigests, _keypair, suiClient);
+      await checkIfKeypairCanBeUsed(
+        undefined,
+        currentFlowDigests,
+        _keypair,
+        suiClient,
+      );
 
       // 1.3) Fund
       let _fundKeypairResult = fundKeypairResult;
