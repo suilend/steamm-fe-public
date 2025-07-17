7a9cd8ee1586ee36a3586dcedabbe0fe53f67cda
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-07-17 18:21:03 +0800

    fix zap for non-sui

--- frontend/src/components/pool/PoolActionsCard.tsx
+++ frontend/src/components/pool/PoolActionsCard.tsx
@@ -112,7 +112,7 @@ function DepositTab({ onDeposit }: DepositTabProps) {
 
   const activeProviders = useMemo(
     () => [
-      // QuoteProvider.AFTERMATH, - Doesn't work with with $intents (?)
+      QuoteProvider.AFTERMATH,
       QuoteProvider.CETUS,
       QuoteProvider._7K,
       QuoteProvider.FLOWX,
