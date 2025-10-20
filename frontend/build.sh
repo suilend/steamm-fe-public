281d91e6ff9af20da2972eedf6de003154ecfc6d
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-10-20 13:21:44 +0800

    update build.sh

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-exit 1;
+echo "Checking if send has changed..."
+
+if [[ "$VERCEL_ENV" == "production" || "$VERCEL_ENV" == "beta" || "$VERCEL_ENV" == "playground" ]] ; then
+    # Proceed with the build
+    echo "🎉 - Build can proceed"
+    exit 1;
+else
+    # Don't build
+    echo "🛑 - Build cancelled"
+    exit 0;
+fi
