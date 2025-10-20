f27c7d82f1ac886f1d9e0afb10ee682800b56661
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-10-20 13:30:20 +0800

    update build.sh

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# Check if environment is production, beta, or playground
 if [[ "$VERCEL_ENV" == "production" || "$VERCEL_ENV" == "beta" || "$VERCEL_ENV" == "playground" ]] ; then
-    # Proceed with the build
-    echo "🎉 - Build can proceed"
+    # Proceed with build
+    echo "🎉 - Environment is production, beta, or playground - proceeding with build"
     exit 1;
 else
-    # Don't build
-    echo "🛑 - Build cancelled"
+    # Skip build
+    echo "🛑 - Environment is not production, beta, or playground - skipping build"
     exit 0;
 fi
