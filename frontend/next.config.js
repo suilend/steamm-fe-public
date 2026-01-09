8050221fdca7dcdc17307fcad2522d24aac31662
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2026-01-09 14:05:45 +0800

    remove X-Frame-Options header

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -40,10 +40,6 @@ module.exports = {
         source: "/:path*",
         headers: [
           {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
             key: "Strict-Transport-Security",
             value: "max-age=31536000",
           },
